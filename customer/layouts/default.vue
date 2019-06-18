1c59efba02cc937a825b5f0903156a2037cb9114
ctoc_takaaki <takaaki.nc.jpn@gmail.com>
2019-06-19 05:51:56 +0900

    ロゴOk,faviconやり直し！！

--- customer/layouts/default.vue
+++ customer/layouts/default.vue
@@ -3,10 +3,10 @@
     <!-- ヘッダー~~~~~~ -->
     <header>
       <nav>
-        <div><router-link to="#home"><img src="/logo.png" width="240px" height="160px" class="logo"></router-link></div>
         <ul>
-          <li><router-link to="#service" class="navigation">SERVICE</router-link></li>
+          <li class="top"><router-link to="#service" class="navigation">SERVICE</router-link></li>
           <li><router-link to="#who" class="navigation">WHO WE ARE</router-link></li>
+          <li><router-link to="#home"><img src="/logo.png" width="150px" height="100px" class="logo"></router-link></li>
           <li><router-link to="#about" class="navigation">ABOUT</router-link></li>
           <li><router-link to="#member" class="navigation">MEMBER</router-link></li>
           <li><router-link to="#contact" class="navigation">CONTACT</router-link></li>
