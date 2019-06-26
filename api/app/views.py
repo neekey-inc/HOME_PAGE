4204d6c5a3acd120daa78e902725bb78d348b4ef
ctoc_takaaki <takaaki.nc.jpn@gmail.com>
2019-06-26 12:58:20 +0900

    アクセス制限完了

--- api/app/views.py
+++ api/app/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets
 from .serializers import ContactSerializer
 from .models import Contact
+from rest_framework.permissions import IsAdminUser
 # from django.contrib.auth import authenticate
 # from django.db import transaction
 # from django.http import HttpResponse, Http404
@@ -13,6 +14,7 @@ class ContactViewSet(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     # send_notification(Contact.objects, '登録')
     queryset = Contact.objects.all()
+    permission_classes = [IsAdminUser]
 
 
 # SLACK_URL = "https://hooks.slack.com/services/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"
